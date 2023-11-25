a55fc2aca7d2e7c9960874558f8b26a1c34621c6
TPABOBAP <png752@gmail.com>
2023-11-25 23:33:37 +0200

    bullets are visible for everyone, but does not move for other users

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Shooting : MonoBehaviour
 {
@@ -21,21 +22,23 @@ public class Shooting : MonoBehaviour
         Ray2D ray = new Ray2D(transform.position, Input.mousePosition);
         Debug.DrawRay(transform.position, Input.mousePosition, Color.red);
 
-        if(canShoot)
-        if (Input.GetMouseButtonDown(0))
+        if (canShoot && Input.GetMouseButtonDown(0))
         {
             canShoot = false;
             Invoke("CanShootTrue", delay);
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // Создание пули через сеть с помощью PhotonNetwork.Instantiate
+            GameObject bullet = PhotonNetwork.Instantiate("Bullet", transform.position, Quaternion.identity);
+
             bullet.GetComponent<Bullet>().Damage = damage;
 
             Vector2 shootDirection = new Vector2(transform.right.x, transform.right.y);
             bullet.GetComponent<Rigidbody2D>().velocity = -shootDirection.normalized * bulletSpeed;
-
-            //bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(
-            //    transform.forward.x + transform.rotation.z,
-            //    transform.forward.y + transform.rotation.z) * bulletSpeed;
         }
+        //bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(
+        //    transform.forward.x + transform.rotation.z,
+        //    transform.forward.y + transform.rotation.z) * bulletSpeed;
+    
         //var hithandlers = target.GetComponents<IHitHandler>();
         //foreach (var i in hithandlers)
         //    i?.Handlehit(new HitInfo() { point = hit.point, force = force, direction = ray.direction, normalno = hit.normal });
