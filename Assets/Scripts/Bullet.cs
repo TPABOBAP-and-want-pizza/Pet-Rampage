47578730c04d9e75920575617f14b2c1b881601d
TPABOBAP <png752@gmail.com>
2023-11-26 00:27:57 +0200

    the bullets started moving, but the direction is calculated locally for the mouse position of each player: (that is, it is incorrect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Bullet : MonoBehaviour
+public class Bullet : MonoBehaviourPunCallbacks
 {
     public int Damage { get; set; }
-    private void Start()
+    public float bulletSpeed = 10f;
+    public float destroyTime = 2f; // Время до уничтожения пули
+
+    void Start()
     {
-        Invoke("ToDestroy", 1f);
+        StartCoroutine(SetVelocity());
+        Invoke("DestroyBullet", destroyTime); // Вызов уничтожения пули через время destroyTime
     }
+
+    IEnumerator SetVelocity()
+    {
+        yield return new WaitForFixedUpdate(); // Ждем следующего FixedUpdate для корректного расчета физики
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 shootDirection = (mousePosition - (Vector2)transform.position).normalized;
+        Debug.Log("Shoot Direction: " + shootDirection); // Вывод значения в лог
+        GetComponent<Rigidbody2D>().velocity = shootDirection * bulletSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<IHitHandler>() != null)
         {
-            if(collision.gameObject.GetComponent<IDamageTaker>() != null)
+            if (collision.gameObject.GetComponent<IDamageTaker>() != null)
             {
                 collision.gameObject.GetComponent<IDamageTaker>().TakeDamage(Damage);
             }
-            Destroy(gameObject);
+            photonView.RPC("DestroyBullet", RpcTarget.AllBuffered);
         }
     }
-    private void ToDestroy()
+
+    [PunRPC]
+    void DestroyBullet()
     {
         Destroy(gameObject);
     }
