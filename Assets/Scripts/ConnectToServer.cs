3604fe6e322a8feb8135c09611407ce89bcaad93
TPABOBAP <png752@gmail.com>
2023-11-25 17:37:12 +0200

    Ебля

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        SceneManager.LoadScene("Menu");
     }
+
 }
