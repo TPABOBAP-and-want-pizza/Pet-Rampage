02050e031ff06516ea333966d3d8a00eeffd8674
TPABOBAP <png752@gmail.com>
2023-11-25 19:55:40 +0200

    add Join and Create room function

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class CreateAndJoinRooms : MonoBehaviour
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public InputField createInput;
+    public InputField joinInput;
+
+    public void CreateRoom()
     {
-        
+        PhotonNetwork.CreateRoom(createInput.text);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void JoinRoom()
     {
-        
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+ 
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("SampleScene");
     }
 }
