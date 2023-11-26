1346be173a51ece37e1437163376c2ce4a58da60
TPABOBAP <png752@gmail.com>
2023-11-26 22:12:37 +0200

    continue trees hell

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -42,33 +42,47 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
             Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
 
             GameObject zombie = PhotonNetwork.InstantiateRoomObject(zombiePrefab.name, randomPosition, Quaternion.identity);
-            //DontDestroyOnLoad(zombie);
+            DontDestroyOnLoad(zombie);
+        }
+    }
+
+    private IEnumerator SpawnTreesWithDelay(List<Vector2> positions)
+    {
+        float delayBetweenTrees = 0.1f; // Задержка между спауном деревьев
+
+        foreach (Vector2 position in positions)
+        {
+            SpawnTreeAtPosition(position);
+            yield return new WaitForSeconds(delayBetweenTrees);
         }
     }
 
     private void SpawnTrees()
     {
-        int treeCount = 100; // ���������� �������� ��� ������
+        int treeCount = 100; // Общее количество деревьев
+        List<Vector2> randomPositions = GenerateRandomPositions(treeCount);
 
-        float Xrange10 = (maxX - minX) / treeCount;
-        float Yrange10 = (maxY - minY) / treeCount;
+        randomPositions.Sort((a, b) => b.y.CompareTo(a.y)); // Сортировка позиций по значению Y в порядке убывания
 
-        for (int i = 0; i < treeCount; i++)
-        {
-            Vector2 randomPosition = new Vector2(Random.Range(0, treeCount) * Xrange10 + minX + Random.Range(-2, 2), Random.Range(0, treeCount) * Yrange10 + minY + Random.Range(-1, 1));
-
-            GameObject tree = PhotonNetwork.InstantiateRoomObject(treePrefab.name, randomPosition, Quaternion.identity);
-            DontDestroyOnLoad(tree);
-
-            // �������� ��������� SpriteRenderer �������
-            SpriteRenderer treeRenderer = tree.GetComponent<SpriteRenderer>();
-            if (treeRenderer != null)
-            {
-                // ������������� Sorting Layer � Sorting Order ��� ����������� ������� �����������
-                treeRenderer.sortingLayerName = "TreesSortingLayer"; // �������� "YourSortingLayer" �� ��� ���� ����������
-                treeRenderer.sortingOrder = i; // ����������� i ��� ����������� ������� ��� ������� ������
-            }
-        }
+        StartCoroutine(SpawnTreesWithDelay(randomPositions));
     }
+
+    private List<Vector2> GenerateRandomPositions(int count)
+    {
+        List<Vector2> positions = new List<Vector2>();
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            positions.Add(randomPosition);
+        }
+        return positions;
+    }
+
+    private void SpawnTreeAtPosition(Vector2 position)
+    {
+        GameObject tree = PhotonNetwork.InstantiateRoomObject(treePrefab.name, position, Quaternion.identity);
+        DontDestroyOnLoad(tree);
+    }
+
 
 }
