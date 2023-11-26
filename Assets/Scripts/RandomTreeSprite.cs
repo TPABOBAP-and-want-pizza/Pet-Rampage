6ebecae5129ea788b7e77efdb5fa3ae91a75aefb
TPABOBAP <png752@gmail.com>
2023-11-26 19:33:50 +0200

    added an empty SpawnTree object with the RandomTreeSprite script, which makes tree branches random.
    trees for all players are of different colors:(    Trees are located in the folder Assets/Resources/Grafic_Resources/Trees

---
+++ Assets/Scripts/RandomTreeSprite.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTreeSprite : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer treeSpriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
+
+        if (treeSpriteRenderer != null)
+        {
+            Sprite[] treeSprites = Resources.LoadAll<Sprite>("Grafic_Resources/Trees");
+
+            if (treeSprites != null && treeSprites.Length > 0)
+            {
+                int randomIndex = Random.Range(0, treeSprites.Length);
+                treeSpriteRenderer.sprite = treeSprites[randomIndex];
+            }
+            else
+            {
+                Debug.LogError("No tree sprites found in the Grafic_Resources/Trees folder.");
+            }
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer not found on the parent object.");
+        }
+    }
+}
