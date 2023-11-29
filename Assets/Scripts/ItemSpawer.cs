e3934493fecffafa729ab070055f5b30fe957a48
want-pizza <olex2005my@gmail.com>
2023-11-30 00:06:54 +0200

    fixed dup items

--- Assets/Scripts/ItemSpawer.cs
+++ Assets/Scripts/ItemSpawer.cs
@@ -7,13 +7,18 @@ public class ItemSpawer : MonoBehaviour
 {
     [SerializeField] private List<GameObject> items = new List<GameObject>();
 
+    private bool spawned = false;
+
     private void OnDestroy()
     {
-        foreach(GameObject item in items)
+        if (PhotonNetwork.IsMasterClient)
         {
-            Debug.Log("Transform Position: " + transform.position);
+            foreach (GameObject item in items)
+            {
+                Debug.Log("Transform Position: " + transform.position);
 
-            PhotonNetwork.Instantiate(item.name, transform.position+new Vector3(Random.Range(-0.5f,0.5f), Random.Range(-0.5f, 0.5f),0.00f), Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(item.name, transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0.00f), Quaternion.identity, 0);
+            }
         }
     }
 }
