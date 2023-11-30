f93875c09ee8851b4fa03d6458b5fba6f9312d2b
TPABOBAP <png752@gmail.com>
2023-11-30 22:57:37 +0200

    drew a melee weapon

--- Assets/Scripts/RandomTreeSprite.cs
+++ Assets/Scripts/RandomTreeSprite.cs
@@ -1,19 +1,13 @@
-using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomTreeSprite : MonoBehaviourPunCallbacks, IPunObservable
+public class RandomTreeSprite : MonoBehaviour
 {
-    private SpriteRenderer treeSpriteRenderer;
-
     void Start()
     {
-        treeSpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer treeSpriteRenderer = transform.GetComponent<SpriteRenderer>();
 
-        SyncTreeColor(); // Вызывайте этот метод для всех игроков
-    }
-
-    private void SyncTreeColor()
-    {
         if (treeSpriteRenderer != null)
         {
             Sprite[] treeSprites = Resources.LoadAll<Sprite>("Grafic_Resources/Trees");
@@ -22,9 +16,6 @@ public class RandomTreeSprite : MonoBehaviourPunCallbacks, IPunObservable
             {
                 int randomIndex = Random.Range(0, treeSprites.Length);
                 treeSpriteRenderer.sprite = treeSprites[randomIndex];
-
-                // Отправляем информацию о выбранном цвете дерева через Photon
-                photonView.RPC("SyncColorRPC", RpcTarget.AllBuffered, randomIndex);
             }
             else
             {
@@ -34,30 +25,6 @@ public class RandomTreeSprite : MonoBehaviourPunCallbacks, IPunObservable
         else
         {
             Debug.LogError("SpriteRenderer not found on the parent object.");
-        }
-    }
-
-    [PunRPC]
-    private void SyncColorRPC(int colorIndex)
-    {
-        Sprite[] treeSprites = Resources.LoadAll<Sprite>("Grafic_Resources/Trees");
-        if (treeSprites != null && treeSprites.Length > colorIndex)
-        {
-            treeSpriteRenderer.sprite = treeSprites[colorIndex];
-        }
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            // Если вы хотите передавать дополнительные данные, отличные от цвета,
-            // используйте stream.SendNext() для записи этих данных.
-        }
-        else
-        {
-            // Если вы хотите считать дополнительные данные, отличные от цвета,
-            // используйте stream.ReceiveNext() для чтения этих данных.
         }
     }
 }
