26e9cfab23e3c6082b3c72904bcda7bd9f0cd34d
want-pizza <olex2005my@gmail.com>
2023-12-03 22:57:19 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,11 +13,6 @@ public class GameManager : MonoBehaviour
     {
         IsNight = isNight;
         night = IsNight;
-        if (!IsNight)
-        {
-            Day++;
-            day = Day;
-        }
     }
     public void SetDay(int day)
     {
