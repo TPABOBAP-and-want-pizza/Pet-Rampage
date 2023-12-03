f3217b4d8e15383103c91fe49f0a2f210f7d38d3
TPABOBAP <png752@gmail.com>
2023-12-04 01:07:10 +0200

    add music tougle in lobby

--- Assets/Scripts/Music and sounds/MusicManager.cs
+++ Assets/Scripts/Music and sounds/MusicManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    private static MusicManager instance;
     public AudioClip[] musicTracks;
     private AudioSource audioSource;
+    private bool isMusicPlaying = true;
 
     private void Start()
     {
@@ -13,16 +14,40 @@ public class MusicManager : MonoBehaviour
         StartCoroutine(PlayMusic());
     }
 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private IEnumerator PlayMusic()
     {
-        while (true)
+        while (isMusicPlaying)
         {
             AudioClip randomTrack = musicTracks[Random.Range(0, musicTracks.Length)];
-
             audioSource.clip = randomTrack;
-
             audioSource.Play();
             yield return new WaitForSeconds(randomTrack.length);
+        }
+    }
+
+    public void ToggleMusic(bool isMusicOn)
+    {
+        isMusicPlaying = isMusicOn;
+        if (!isMusicOn)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
+            StartCoroutine(PlayMusic());
         }
     }
 }
