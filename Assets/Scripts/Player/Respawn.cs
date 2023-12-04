a62e99e5b683cb9cb39e964a2eac4c6babc7d945
TPABOBAP <png752@gmail.com>
2023-12-04 11:18:34 +0200

    maybe fix respawn

--- Assets/Scripts/Player/Respawn.cs
+++ Assets/Scripts/Player/Respawn.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Respawn : MonoBehaviourPun
+public class Respawn : MonoBehaviourPunCallbacks
 {
     [SerializeField] float cooldown = 10f;
     [SerializeField] RespawnTimer timer;
+
+    private PhotonView _photonView;
+
     private void Start()
     {
-        GameObject timerObject = GameObject.FindGameObjectWithTag("Timer");
+        // Получение ссылки на photonView после респауна
+        _photonView = GetComponent<PhotonView>();
 
+        GameObject timerObject = GameObject.FindGameObjectWithTag("Timer");
         if (timerObject != null)
         {
             timer = timerObject.GetComponent<RespawnTimer>();
@@ -21,9 +26,10 @@ public class Respawn : MonoBehaviourPun
             Debug.LogError("Timer object not found!");
         }
     }
+
     private void OnDestroy()
     {
-        if (photonView.IsMine && !GameManager.IsGameOver)
+        if (_photonView.IsMine && !GameManager.IsGameOver)
         {
             timer.transform.parent.gameObject.SetActive(true);
             timer.StartTimer(cooldown);
