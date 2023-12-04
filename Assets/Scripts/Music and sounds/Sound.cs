2e743070fe527545c9aa6a16eb8f15faa673dd18
want-pizza <olex2005my@gmail.com>
2023-12-04 03:42:07 +0200

    added apple sound

--- Assets/Scripts/Music and sounds/Sound.cs
+++ Assets/Scripts/Music and sounds/Sound.cs
@@ -7,12 +7,36 @@ public class Sound : MonoBehaviourPun
 {
     public AudioClip[] sounds;
 
+    public bool IsPlaying { get; set; } = false;
     private AudioSource audioSrc => GetComponent<AudioSource>();
-    public void PlaySound(AudioClip clip, float volume = 1f, bool destroyed = false, float p1 = 0.85f, float p2 = 1.2f) 
+    private Coroutine soundCoroutine;
+    public void PlayRandomSound(float volume = 1f, bool destroyed = false, float p1 = 0.85f, float p2 = 1.2f)
     {
-        audioSrc.pitch = Random.Range(p1, p2);
-        audioSrc.volume = volume;
-        audioSrc.clip = clip;
-        audioSrc.Play();
+        if (!IsPlaying)
+        {
+            Debug.Log("PlaySound");
+            IsPlaying = true;
+            if(audioSrc == null)
+            {
+                Debug.Log("bruh");
+                return;
+            }
+                
+            audioSrc.pitch = Random.Range(p1, p2);
+            audioSrc.volume = volume;
+            audioSrc.clip = sounds[Random.Range(0, sounds.Length)];
+            audioSrc.Play();
+
+            soundCoroutine = StartCoroutine(WaitForSound());
+        }
+    }
+
+    private IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(audioSrc.clip.length);
+
+        IsPlaying = false;
+
+        soundCoroutine = null;
     }
 }
